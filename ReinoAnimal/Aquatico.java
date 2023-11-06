f5afe879c72d2c9eae741a54d4e2d5e9a8675ba1
Gabriel Richard <gabrielrichard560@gmail.com>
2023-11-06 20:13:30 -0300

    Criação da classe Aquatico

---
+++ ReinoAnimal/Aquatico.java
@@ -0,0 +1,80 @@
+package ReinoAnimal;
+
+/**
+ * Esta classe representa um animal aquático
+ * 
+ * @author Gabriel Richard Morais de Lima
+ *
+ */
+
+public class Aquatico extends Animal {
+
+	private String tipoAgua;
+	private boolean escamas;
+
+	/**
+	 * Construtor da classe Aquatico
+	 * 
+	 * @param especie
+	 * @param sexo
+	 * @param dataNascimento
+	 * @param tipoAgua
+	 * @param escamas
+	 */
+
+	public Aquatico(String especie, String sexo, double dataNascimento, String tipoAgua, boolean escamas) {
+		super(especie, sexo, dataNascimento);
+		this.tipoAgua = tipoAgua;
+		this.escamas = escamas;
+
+		// TODO Auto-generated constructor stub
+	}
+
+	public void nadar() {
+
+		/**
+		 * Imprime no console que o animal está nadando
+		 * 
+		 * @since 06/11/2023
+		 */
+		System.out.println("O animal está nadando...");
+
+	}
+
+	public void comer() {
+
+		System.out.println("O animal aquatico está comendo...");
+	}
+
+	@Override
+	public void perfil() {
+
+		/**
+		 * Imprime no console os dados do animal
+		 * 
+		 * @since 06/11/2023
+		 * 
+		 **/
+
+		super.perfil();
+		System.out.println("Tipo de água: " + tipoAgua + "Escamoso: " + escamas);
+
+	}
+
+	public String getTipoAgua() {
+		return tipoAgua;
+	}
+
+	public void setTipoAgua(String tipoAgua) {
+		this.tipoAgua = tipoAgua;
+	}
+
+	public boolean isEscamas() {
+		return escamas;
+	}
+
+	public void setEscamas(boolean escamas) {
+		this.escamas = escamas;
+	}
+
+}
