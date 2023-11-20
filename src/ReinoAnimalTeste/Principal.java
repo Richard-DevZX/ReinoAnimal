69fdc13db063d8c738c8002e73c9efba81929d7f
Richard-DevZX <gabrielrichard560@gmail.com>
2023-11-20 11:13:00 -0300

    Consertando Projeto para documentação

--- src/ReinoAnimalTeste/Principal.java
+++ src/ReinoAnimalTeste/Principal.java
@@ -1,5 +1,4 @@
 package ReinoAnimalTeste;
-
 import ReinoAnimal.Aereo;
 import ReinoAnimal.Animal;
 import ReinoAnimal.Aquatico;
@@ -11,8 +10,11 @@ import ReinoAnimal.Terrestre;
  * @author Gabriel Richard Morais de Lima
  *
  */
-public class Principal {
 
+
+
+public class Principal {
+	
 	/**
 	 * Método que é utilizado para realizar testes
 	 * 
@@ -21,19 +23,19 @@ public class Principal {
 
 	public static void main(String[] args) {
 
-		Aquatico aqua1.0 = new Aquatico("Rhincodon typus", "fêmea", 3, "água salgada", false);
+		Aquatico aqua1 = new Aquatico("Rhincodon typus", "fêmea", 3, "água salgada", false);
 		
-		Aquatico aqua2.0 = new Aquatico("Pseudocrocidolita", "macho", 1, "água doce", true);
+		Aquatico aqua2 = new Aquatico("Pseudocrocidolita", "macho", 1, "água doce", true);
 
 		Terrestre earth = new Terrestre("Phantera leo", "macho", 2, "africa", true);
 		
 		Terrestre earth2 = new Terrestre("Hydrochoerus hydrochaeris", "fêmea", 6, "América do Sul", true);
 
-		Aereo air1.0 = new Aereo("Trochilus", "macho", 1, 40.5);
+		Aereo air1 = new Aereo("Trochilus", "macho", 1, 40.5);
 		
-		Aereo air2.0 = new Aereo("Cyanopsitta spixii", "fêmea", 7, 50.9);
+		Aereo air2 = new Aereo("Cyanopsitta spixii", "fêmea", 7, 50.9);
 
-		aqua.comer();
+		aqua1.comer();
 
 		aqua2.nadar();
 
@@ -41,7 +43,7 @@ public class Principal {
 
 		earth2.mover();
 
-		air.comer();
+		air1.comer();
 
 		air2.voar();
 
